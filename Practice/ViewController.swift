82c4247a9d155665cb4f0f5c219e27b6a4cc64da
Tsering Lama <tseringlama@pursuit.org>
2019-12-24 17:13:17 -0500

    Initial Commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Tsering Lama on 12/24/19.
+//  Copyright © 2019 Tsering Lama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
